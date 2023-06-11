6f5a834de0d82a247054341ee4c847a683142e6e
agpavlik <agpavlik@gmail.com>
2023-06-11 16:56:06 -0400

    Add content on the HomePage.js and min style in HomePage.css

---
+++ client/src/routes/HomeRoute.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomePage from '../components/HomePage';
+
+const HomeRoute = () => {
+
+  return (
+    <div className="home-route">
+      <HomePage/>
+    </div>
+  )
+
+}
+
+export default HomeRoute;
