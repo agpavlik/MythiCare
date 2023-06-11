218ec5ee7e1947341e3f4667216eda7e1e8e8ce7
agpavlik <agpavlik@gmail.com>
2023-06-11 15:00:47 -0400

    Add basic pages

---
+++ client/src/components/HomePage.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./HomePage.css";
+
+
+const HomePage = () => {
+
+
+return (
+  <section className = "home-section">
+
+    <div className="home-box">
+      <img className="logo" alt="logo image"/>
+    </div>
+
+    <div className="home-box">
+
+    </div>
+
+    <div className="home-box">
+
+    </div>
+
+    <div className="home-box">
+
+    </div>
+
+  </section>
+)
+
+
+
+}
+
+export default HomePage;
--- server/app.js
+++ server/app.js
@@ -1,20 +1,40 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
-var app = express();
+// var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
-module.exports = app;
+// module.exports = app;
+
+require('dotenv').config()
+const {ENVIROMENT, PORT} = process.env;
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// middleware setup
+app.use(morgan(ENVIROMENT));
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+	res.json({greetings: 'hello world'});
+})
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+
