240318b5377d1197c8ddb23f8dd4c81b2367abb3
ayan-hassan <ahassan@hotmail.ca>
2023-06-20 22:26:44 -0400

    style updates

--- client/src/components/SittersPage.jsx
+++ client/src/components/SittersPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import AvailabilityCalendar from './AvailabilityCalendar';
+import Layout from './layout';
 
 const SittersPage = () => {
   const [sitter, setSitter] = useState([])
@@ -21,20 +22,25 @@ const SittersPage = () => {
   }, [id]);
 
   return (
-    <div>
-      <h2>{sitter.first_name} {sitter.last_name}</h2>
-      <article id='sitter'>
-        <img src={sitter.profile_photo} alt={sitter.first_name} className="sitter--photo"/>
-        <div className="sitter--info">
-          <div>{sitter.bio}</div>
-          <div>{sitter.city}, {sitter.country}</div>
-          Experience: {sitter.experience} years
-          <div>from ${sitter.nightly_rate} per night</div>
-          <p>Avg. Rating: {sitter.avg_rating}</p>
-        </div>
-    </article>
-      <AvailabilityCalendar sitterId={sitter.sitter_id} nightly_rate={sitter.nightly_rate}/>
-    </div>
+    <Layout>
+
+      <div>
+        <h2>{sitter.first_name} {sitter.last_name}</h2>
+        <article id='sitter-page'>
+          <div className='sitter-details'>
+            <img src={sitter.profile_photo} alt={sitter.first_name} className="sitter--photo"/>
+            <div className="sitter--info">
+              <div>{sitter.bio}</div>
+              <div>{sitter.city}, {sitter.country}</div>
+              Experience: {sitter.experience} years
+              <div>from ${sitter.nightly_rate} per night</div>
+              <p>Avg. Rating: {sitter.avg_rating}</p>
+            </div>
+          </div>
+          <div className='calendar'><AvailabilityCalendar sitterId={sitter.sitter_id} nightly_rate={sitter.nightly_rate}/></div>
+        </article>
+      </div>
+    </Layout>
   );
 };
 
--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,8 +1,11 @@
 const { Pool } = require('pg')
+
+const {DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT} = process.env;
+
 const pool = new Pool({
   user: 'development',
   host: 'localhost',
-  database: 'sitter',
+  database: DB_DATABASE,
   password: 'development',
   port: 5432,
 })
