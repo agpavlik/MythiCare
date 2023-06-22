e0e0f1f54174dec0a67cc4b03e366c8d8f272d57
ayan-hassan <ahassan@hotmail.ca>
2023-06-22 12:54:54 -0400

    home page styling

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -14,8 +14,7 @@ const About = () => {
           <li>Utilize our robust profiling system to create profiles for your specific pets, including information such as their vaccination status and their typical daily consumption of children.</li>
           <li>Connect with pet sitters who can handle your pets' unique needs and are located in your area. Alternatively, become a pet sitter yourself and tame a new beast to your whim.</li>
           <li>Conveniently and securely book pet sitting services.</li>
-          <li>Receive updates and photos of your beloved pets during the sitting period.</li>
-          <li>Discover local pet-friendly establishments, including parks, groomers, and veterinarians.</li>
+          <li>Receive updates and photos of your beloved pets during the sitting period.</li> 
         </ul>
         <p>Our team of dedicated developers and pet enthusiasts have worked tirelessly under the watchful eye of our Lighthouse Labs overlords to bring this app to you today!</p>
         <p>Enjoy peace of mind knowing that your pets are in good hands with Mythicare, our Pet Sitter app!</p>
